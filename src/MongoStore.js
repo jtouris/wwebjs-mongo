c33e1ce9c1441dd1d3a05b4356541dca20545b31
jtourisNS <jtouris@netsuite.com>
2022-07-09 21:30:05 -0300

    [RemoteAuth] - Update on remoteAuth now makes stores handle deleting previous session after saving

--- src/MongoStore.js
+++ src/MongoStore.js
@@ -7,26 +7,28 @@ class MongoStore {
     }
 
     async sessionExists(options) {
-        let multiDeviceCollection = this.mongoose.connection.db.collection(`multidevice-${options.session}.files`);
+        let multiDeviceCollection = this.mongoose.connection.db.collection(`whatsapp-${options.session}.files`);
         let hasExistingSession = await multiDeviceCollection.countDocuments();
         return !!hasExistingSession;   
     }
     
     async save(options) {
         var bucket = new this.mongoose.mongo.GridFSBucket(this.mongoose.connection.db, {
-            bucketName: `multidevice-${options.session}`
+            bucketName: `whatsapp-${options.session}`
         });
-        return new Promise((resolve, reject) => {
+        await new Promise((resolve, reject) => {
             fs.createReadStream(`${options.session}.zip`)
                 .pipe(bucket.openUploadStream(`${options.session}.zip`))
                 .on('error', err => reject(err))
                 .on('close', () => resolve());
         });
+        options.bucket = bucket;
+        await this.#deletePrevious(options);
     }
 
     async extract(options) {
         var bucket = new this.mongoose.mongo.GridFSBucket(this.mongoose.connection.db, {
-            bucketName: `multidevice-${options.session}`
+            bucketName: `whatsapp-${options.session}`
         });
         return new Promise((resolve, reject) => {
             bucket.openDownloadStreamByName(`${options.session}.zip`)
@@ -38,7 +40,7 @@ class MongoStore {
 
     async delete(options) {
         var bucket = new this.mongoose.mongo.GridFSBucket(this.mongoose.connection.db, {
-            bucketName: `multidevice-${options.session}`
+            bucketName: `whatsapp-${options.session}`
         });
         const documents = await bucket.find({
             filename: `${options.session}.zip`
@@ -47,6 +49,16 @@ class MongoStore {
         documents.map(async doc => {
             return bucket.delete(doc._id);
         });   
+    }
+
+    async #deletePrevious(options) {
+        const documents = await options.bucket.find({
+            filename: `${options.session}.zip`
+        }).toArray();
+        if (documents.length > 1) {
+            const oldSession = documents.reduce((a, b) => a.uploadDate < b.uploadDate ? a : b);
+            return options.bucket.delete(oldSession._id);   
+        }
     }
 }
 
